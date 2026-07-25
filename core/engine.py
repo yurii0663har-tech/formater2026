1137b9f384965639037abad68053641bdb371ff4
adamlit88 <yurii0663.har@gmail.com>
2026-07-25 12:45:41 +0200

    Add class inheritance formatting support

--- core/engine.py
+++ core/engine.py
@@ -31,36 +31,36 @@ class FormatterEngine(ast.NodeVisitor):
 
 
     # -------------------------
-    # Functions
+    # MODULE
     # -------------------------
 
-    def visit_FunctionDef(self, node):
-
-        args = ", ".join(
-            arg.arg for arg in node.args.args
-        )
-
-        self.write(
-            f"def {node.name}({args}):"
-        )
-
-        self.level += 1
+    def visit_Module(self, node):
 
         for item in node.body:
             self.visit(item)
 
-        self.level -= 1
-
 
     # -------------------------
-    # Classes
+    # CLASS
     # -------------------------
 
     def visit_ClassDef(self, node):
 
-        self.write(
-            f"class {node.name}:"
-        )
+        if node.bases:
+
+            bases = ", ".join(
+                ast.unparse(base)
+                for base in node.bases
+            )
+
+            header = f"class {node.name}({bases}):"
+
+        else:
+
+            header = f"class {node.name}:"
+
+
+        self.write(header)
 
         self.level += 1
 
@@ -71,104 +71,15 @@ class FormatterEngine(ast.NodeVisitor):
 
 
     # -------------------------
-    # Variables
+    # FUNCTION
     # -------------------------
 
-    def visit_Assign(self, node):
+    def visit_FunctionDef(self, node):
+
+        args = ast.unparse(node.args)
 
         self.write(
-            ast.unparse(node)
-        )
-
-
-    # -------------------------
-    # Simple expressions
-    # -------------------------
-
-    def visit_Expr(self, node):
-
-        self.write(
-            ast.unparse(node)
-        )
-
-
-    # -------------------------
-    # IF / ELSE / ELIF
-    # -------------------------
-
-    def visit_If(self, node):
-
-        self.write(
-            f"if {ast.unparse(node.test)}:"
-        )
-
-        self.level += 1
-
-        for item in node.body:
-            self.visit(item)
-
-        self.level -= 1
-
-
-        if node.orelse:
-
-            # elif
-            if (
-                len(node.orelse) == 1
-                and isinstance(node.orelse[0], ast.If)
-            ):
-
-                self.write(
-                    f"elif {ast.unparse(node.orelse[0].test)}:"
-                )
-
-                self.level += 1
-
-                for item in node.orelse[0].body:
-                    self.visit(item)
-
-                self.level -= 1
-
-
-            # else
-            else:
-
-                self.write("else:")
-
-                self.level += 1
-
-                for item in node.orelse:
-                    self.visit(item)
-
-                self.level -= 1
-
-
-    # -------------------------
-    # FOR loop
-    # -------------------------
-
-    def visit_For(self, node):
-
-        self.write(
-            f"for {ast.unparse(node.target)} in {ast.unparse(node.iter)}:"
-        )
-
-        self.level += 1
-
-        for item in node.body:
-            self.visit(item)
-
-        self.level -= 1
-
-
-    # -------------------------
-    # WHILE loop
-    # -------------------------
-
-    def visit_While(self, node):
-
-        self.write(
-            f"while {ast.unparse(node.test)}:"
+            f"def {node.name}({args}):"
         )
 
         self.level += 1
@@ -197,43 +108,50 @@ class FormatterEngine(ast.NodeVisitor):
 
 
     # -------------------------
-    # PASS
+    # EXPRESSIONS
     # -------------------------
 
-    def visit_Pass(self, node):
-
-        self.write("pass")
-
-
-    # -------------------------
-    # BREAK
-    # -------------------------
-
-    def visit_Break(self, node):
-
-        self.write("break")
-
-
-    # -------------------------
-    # CONTINUE
-    # -------------------------
-
-    def visit_Continue(self, node):
-
-        self.write("continue")
-        
-    def visit_Import(self, node):
+    def visit_Expr(self, node):
 
         self.write(
-            ast.unparse(node)
+            ast.unparse(node.value)
         )
-        
-    def visit_ImportFrom(self, node):
+
+
+    # -------------------------
+    # IF
+    # -------------------------
+
+    def visit_If(self, node):
 
         self.write(
-            ast.unparse(node)
+            f"if {ast.unparse(node.test)}:"
         )
-        
+
+        self.level += 1
+
+        for item in node.body:
+            self.visit(item)
+
+        self.level -= 1
+
+
+        if node.orelse:
+
+            self.write("else:")
+
+            self.level += 1
+
+            for item in node.orelse:
+                self.visit(item)
+
+            self.level -= 1
+
+
+    # -------------------------
+    # FOR
+    # -------------------------
+
     def visit_For(self, node):
 
         self.write(
@@ -246,20 +164,12 @@ class FormatterEngine(ast.NodeVisitor):
             self.visit(item)
 
         self.level -= 1
-    
-    def visit_For(self, node):
 
-        self.write(
-            f"for {ast.unparse(node.target)} in {ast.unparse(node.iter)}:"
-    )
 
-        self.level += 1
+    # -------------------------
+    # WHILE
+    # -------------------------
 
-        for item in node.body:
-            self.visit(item)
-
-        self.level -= 1
-        
     def visit_While(self, node):
 
         self.write(
@@ -274,8 +184,10 @@ class FormatterEngine(ast.NodeVisitor):
         self.level -= 1
 
 
+    # -------------------------
+    # TRY
+    # -------------------------
 
-        
     def visit_Try(self, node):
 
         self.write("try:")
@@ -290,14 +202,9 @@ class FormatterEngine(ast.NodeVisitor):
 
         for handler in node.handlers:
 
-            if handler.type:
-                self.write(
-                    f"except {ast.unparse(handler.type)}:"
-                )
-            else:
-                self.write(
-                    "except:"
-                )
+            self.write(
+                f"except {ast.unparse(handler.type)}:"
+            )
 
             self.level += 1
 
@@ -317,57 +224,26 @@ class FormatterEngine(ast.NodeVisitor):
                 self.visit(item)
 
             self.level -= 1
-    
-        def visit_Try(self, node):
-
-            self.write("try:")
-
-            self.level += 1
-
-            for item in node.body:
-                self.visit(item)
-
-                self.level -= 1
 
 
-                for handler in node.handlers:
+    # -------------------------
+    # WITH
+    # -------------------------
 
-                    if handler.type:
-                        self.write(
-                            f"except {ast.unparse(handler.type)}:"
-                    )
-                    else:
-                        self.write(
-                        "except:"
-                        )
-
-                self.level += 1
-
-                for item in handler.body:
-                    self.visit(item)
-
-                self.level -= 1
-
-
-            if node.finalbody:
-
-                self.write("finally:")
-
-                self.level += 1
-
-                for item in node.finalbody:
-                    self.visit(item)
-
-                self.level -= 1
-                
     def visit_With(self, node):
 
         items = []
 
         for item in node.items:
-            items.append(
-                ast.unparse(item)
-            )
+
+            text = ast.unparse(item.context_expr)
+
+            if item.optional_vars:
+
+                text += f" as {ast.unparse(item.optional_vars)}"
+
+            items.append(text)
+
 
         self.write(
             f"with {', '.join(items)}:"
@@ -375,8 +251,43 @@ class FormatterEngine(ast.NodeVisitor):
 
         self.level += 1
 
-        for stmt in node.body:
-            self.visit(stmt)
+        for item in node.body:
+            self.visit(item)
 
         self.level -= 1
+
+
+    # -------------------------
+    # IMPORTS
+    # -------------------------
+
+    def visit_Import(self, node):
+
+        self.write(
+            ast.unparse(node)
+        )
+
+
+    def visit_ImportFrom(self, node):
+
+        self.write(
+            ast.unparse(node)
+        )
+
+        # -------------------------
+    # ASSIGN
+    # -------------------------
+
+    def visit_Assign(self, node):
+
+        self.write(
+            ast.unparse(node)
+        )
+    # -------------------------
+    # PASS
+    # -------------------------
+
+    def visit_Pass(self, node):
+
+        self.write("pass")
         

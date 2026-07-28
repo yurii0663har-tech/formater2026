909fca6b663a13be85073a059bd785747455ea9d
adamlit88 <yurii0663.har@gmail.com>
2026-07-28 13:53:01 +0200

    Add formatter idempotency test for dict comprehensions

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -2315,7 +2315,25 @@ def build(values):
         self.assertEqual(
             formatted_once,
             formatted_twice
-        )                                        
+        )  
+    def test_formatter_idempotency_dict_comprehension(self):
+
+        code = """
+def build(values):
+    return {x: x * 2 for x in values}
+"""
+
+        formatted_once = self.formatter.format(code)
+
+        formatted_twice = self.formatter.format(
+            formatted_once
+        )
+
+        self.assertEqual(
+            formatted_once,
+            formatted_twice
+        )    
+                                              
 if __name__ == "__main__":
         unittest.main()
 
